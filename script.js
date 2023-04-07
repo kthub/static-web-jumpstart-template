4f467ec3591673a0f93b08a4e6e52ed3e650ded2
Keiichi Tsuda <keiichi.tsuda@gmail.com>
2023-04-07 14:19:25 +0900

    commit

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
+// [3] Javascript sample
+let code = document.getElementsByTagName('code');
 
-var copy = function(target) {
-    var textArea = document.createElement('textarea')
-    textArea.setAttribute('style','width:1px;border:0;opacity:0;')
-    document.body.appendChild(textArea)
-    textArea.value = target.innerHTML
-    textArea.select()
-    document.execCommand('copy')
-    document.body.removeChild(textArea)
-}
+let button = document.createElement("button")
+button.innerHTML = "change contents"
 
-var code = document.getElementsByTagName('code');
-//alert(code.item(0));
-
-var button = document.createElement("button")
-button.className = "btn btn-sm"
-button.innerHTML = "copy"
 code.item(0).parentNode.insertBefore(button, null)
+
 button.addEventListener('click', function(e){
   e.preventDefault()
-  copy(code.item(0))
+  code.item(0).innerHTML = "changed"
 })
-
